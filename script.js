d3688c9c92706f067528313a5312bdc44b119eb3
Makson <maksonsantoss4@gmail.com>
2022-11-29 18:00:58 -0300

    Finish

--- script.js
+++ script.js
@@ -72,10 +72,7 @@ class Transacao {
         return "Depósito realizado com sucesso!"
     }
 
-    static pagamentoSalario (contaOrigem, contaDestino, idPagamento, dataDoPagamento, valorDoSalario){
-        //verificar se a contaOrigem é uma instância da classe Pessoa
-        //Caso seja, o valorDoSalario não pode ser maior que 1000
-        //Caso não passe nesta validação deve retornar: "Seu limite máximo para este tipo de operação é de 1000, entre em contato com o banco!"
+    static pagamentoSalario (contaOrigem, contaDestino, idPagamento, dataDoPagamento, valorDoSalario){        
         if(contaOrigem instanceof Pessoa && valorDoSalario > 1000){
             return "Seu limite máximo para este tipo de operação é de 1000, entre em contato com o banco!"
         }
@@ -106,15 +103,15 @@ class Transacao {
     }
 }
 
-const ChurrosVictor = new Empresa("01", "Vip", "16/05/2022", 156, 2798, 987654321, 1000, "Churros do Victor", "1011121314", "churrosVictor@email.com.br", 1234567890, "16/05/2022");
+const Victor = new Empresa("01", "Vip", "16/05/2022", 156, 2798, 987654321, 1000, "Churros do Victor", "1011121314", "Victor@email.com.br", 1234567890, "16/05/2022");
 
 const Joao = new Pessoa("02", "Comum", "16/03/2022", 156, 2798, 91919292, 100, "João", "2345678901", "joao@kenzie.com.br", "90919293", "01/01/2019");
 
 const Maria = new Pessoa("03", "Vip", "16/03/2022", 156, 2798, 10121416, 5000, "Maria", "3456789012", "maria@kenzie.com.br", "90919293", "01/01/2019");
 
-console.log(ChurrosVictor.saldo); // Deve retornar 1000
-console.log(ChurrosVictor.nomeFantasia); // Deve retornar ChurrosVictor
-console.log(ChurrosVictor.historico); // Deve retornar []
+console.log(Victor.saldo); // Deve retornar 1000
+console.log(Victor.nomeFantasia); // Deve retornar Victor
+console.log(Victor.historico); // Deve retornar []
 console.log(Joao.nome); // Deve retornar João
 console.log(Joao.saldo); // Deve retornar 100
 console.log(Maria.idCliente); // Deve retornar "03"
@@ -122,18 +119,18 @@ console.log(Maria.tipoCliente); // Deve retornar "Vip"
 
 console.log(Transacao.transferencia(Joao, Maria, 1, "25/07/2022", 200)); // Deve retornar {mensagem: "Saldo insuficiente para transferência!"}
 
-console.log(Transacao.transferencia(ChurrosVictor, Joao, 2, "20/07/2022", 500)); // Deve retornar {mensagem: "Transferência realizada com sucesso!"}
+console.log(Transacao.transferencia(Victor, Joao, 2, "20/07/2022", 500)); // Deve retornar {mensagem: "Transferência realizada com sucesso!"}
 
-console.log(ChurrosVictor.historico); // Deve retornar [{idTransacao: 2, dataDeTransacao: "20/07/2022", valorDaTransferencia: 500, tipo: "pagamento"}]
+console.log(Victor.historico); // Deve retornar [{idTransacao: 2, dataDeTransacao: "20/07/2022", valorDaTransferencia: 500, tipo: "pagamento"}]
 console.log(Joao.historico); // Deve retornar [{idTransacao: 2, dataDeTransacao: "20/07/2022", valorDaTransferencia: 500, tipo: "recebimento"}]
 
-console.log(ChurrosVictor.saldo); // Deve retornar 500
+console.log(Victor.saldo); // Deve retornar 500
 console.log(Joao.saldo); // Deve retornar 600
 
-console.log(Transacao.deposito(ChurrosVictor, 3, "02/07/2022", 650)); // Deve retornar {mensagem: "Depósito realizado com sucesso!"}
+console.log(Transacao.deposito(Victor, 3, "02/07/2022", 650)); // Deve retornar {mensagem: "Depósito realizado com sucesso!"}
 
-console.log(ChurrosVictor.historico[1]); // Deve retornar [{idDeposito: 3, dataDoDeposito: "02/07/2022", valorDoDeposito: 650, tipo: "recebimento"}]
-console.log(ChurrosVictor.saldo); // Deve retornar 1150
+console.log(Victor.historico[1]); // Deve retornar [{idDeposito: 3, dataDoDeposito: "02/07/2022", valorDoDeposito: 650, tipo: "recebimento"}]
+console.log(Victor.saldo); // Deve retornar 1150
 
 console.log(Transacao.pagamentoSalario(Maria, Joao, 4, "22/07/2022", 1100)); // Deve retornar {mensagem: "Seu limite máximo para este tipo de operação é de 1000, entre em contato com o banco!"}
 console.log(Transacao.pagamentoSalario(Joao, Maria, 5, "23/07/2022", 700)); // Deve retornar {mensagem: "Saldo insuficiente para realizar o pagamento!"}
